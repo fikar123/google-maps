4d25a8b0a64fa70f5e731886a275aec54ed87499
maxim.novak <maxim.novak@40e51f83-42d8-3811-3182-f19c87b2c331>
2011-05-18 22:45:50 +0000

    Serilaization factory now non-static class

---
+++ trunk/GoogleMapsApi/Serialization/XmlObjectSerializerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using GoogleMapsApi.Common;
+
+namespace GoogleMapsApi.Serialization
+{
+	internal class XmlObjectSerializerFactory : IObjectSerializerFactory
+	{
+		public XmlObjectSerializer GetSerializer<TResponseType>(ResponseOutputType outputType) where TResponseType : class
+		{
+			XmlObjectSerializer serializer;
+			switch (outputType)
+			{
+				case ResponseOutputType.XML:
+					serializer = new DataContractSerializer(typeof(TResponseType));
+					break;
+				case ResponseOutputType.JSON:
+					serializer = new DataContractJsonSerializer(typeof(TResponseType));
+
+					break;
+				default:
+					throw new ArgumentException(outputType + " is unknown output type.");
+			}
+
+			return serializer;
+		}
+	}
+}
