82469fc6af5349d72095ab04f51840cfd4a00f3a
maxim.novak <maxim.novak@40e51f83-42d8-3811-3182-f19c87b2c331>
2012-04-29 22:02:56 +0000

    Main identetion

--- trunk/MapsApiTest/Program.cs
+++ trunk/MapsApiTest/Program.cs
@@ -22,44 +22,44 @@ namespace MapsApiTest
 	{
 		static void Main(string[] args)
 		{
-//Static class use (Directions)
-DirectionsRequest directionsRequest = new DirectionsRequest()
-{
-	Origin = "NYC, 5th and 39",
-	Destination = "Philladephia, Chesnut and Wallnut",
-};
+			//Static class use (Directions)
+			DirectionsRequest directionsRequest = new DirectionsRequest()
+			{
+				Origin = "NYC, 5th and 39",
+				Destination = "Philladephia, Chesnut and Wallnut",
+			};
 
-DirectionsResponse directions = MapsAPI.GetDirections(directionsRequest);
+			DirectionsResponse directions = MapsAPI.GetDirections(directionsRequest);
 
-Console.WriteLine(directions);
-
-
-//Instance class use (Geocode)
-GeocodingRequest geocodeRequest = new GeocodingRequest()
-{
-	Address = "new york city",
-};
-
-GeocodingEngine geocodingEngine = new GeocodingEngine();
-
-GeocodingResponse geocode = geocodingEngine.GetGeocode(geocodeRequest);
-
-Console.WriteLine(geocode);
+			Console.WriteLine(directions);
 
 
-// Static maps API - get static map of with the path of the directions request
-StaticMapsEngine staticMapGenerator = new StaticMapsEngine();
+			//Instance class use (Geocode)
+			GeocodingRequest geocodeRequest = new GeocodingRequest()
+			{
+				Address = "new york city",
+			};
 
-//Path from previos directions request
-IEnumerable<Step> steps = directions.Routes.First().Legs.First().Steps;
-// All start locations
-IList<ILocation> path = steps.Select(step => step.StartLocation).ToList<ILocation>();
-// also the end location of the last step
-path.Add(steps.Last().EndLocation);
+			GeocodingEngine geocodingEngine = new GeocodingEngine();
 
-string url = staticMapGenerator.GenerateStaticMapURL(new StaticMapRequest(new Location(40.38742, -74.55366), 9, new ImageSize(800, 400))
-{
-	Pathes = new List<Path>(){ new Path()
+			GeocodingResponse geocode = geocodingEngine.GetGeocode(geocodeRequest);
+
+			Console.WriteLine(geocode);
+
+
+			// Static maps API - get static map of with the path of the directions request
+			StaticMapsEngine staticMapGenerator = new StaticMapsEngine();
+
+			//Path from previos directions request
+			IEnumerable<Step> steps = directions.Routes.First().Legs.First().Steps;
+			// All start locations
+			IList<ILocation> path = steps.Select(step => step.StartLocation).ToList<ILocation>();
+			// also the end location of the last step
+			path.Add(steps.Last().EndLocation);
+
+			string url = staticMapGenerator.GenerateStaticMapURL(new StaticMapRequest(new Location(40.38742, -74.55366), 9, new ImageSize(800, 400))
+			{
+				Pathes = new List<Path>(){ new Path()
 	{
 		Style = new PathStyle()
 		{
@@ -69,29 +69,29 @@ string url = staticMapGenerator.GenerateStaticMapURL(new StaticMapRequest(new Lo
 	}}
 
 
-});
+			});
 
-Console.WriteLine("Map with path: " + url);
+			Console.WriteLine("Map with path: " + url);
 
 
 
-//Instance class - Async! (Elevation)
-ElevationRequest elevationRequest = new ElevationRequest()
-{
-	Locations = new Location[] { new Location(54, 78) },
-};
+			//Instance class - Async! (Elevation)
+			ElevationRequest elevationRequest = new ElevationRequest()
+			{
+				Locations = new Location[] { new Location(54, 78) },
+			};
 
-ElevationEngine elevationEngine = new ElevationEngine();
+			ElevationEngine elevationEngine = new ElevationEngine();
 
-elevationEngine.BeginGetElevation(elevationRequest,
-																	ar =>
-																	{
-																		ElevationResponse elevation = elevationEngine.EndGetElevation(ar);
-																		Console.WriteLine(elevation);
-																	},
-																	null);
+			elevationEngine.BeginGetElevation(elevationRequest,
+																				ar =>
+																				{
+																					ElevationResponse elevation = elevationEngine.EndGetElevation(ar);
+																					Console.WriteLine(elevation);
+																				},
+																				null);
 
-Console.WriteLine("Finised! (But wait .. async elevation request should get response soon)");
+			Console.WriteLine("Finised! (But wait .. async elevation request should get response soon)");
 
 
 
